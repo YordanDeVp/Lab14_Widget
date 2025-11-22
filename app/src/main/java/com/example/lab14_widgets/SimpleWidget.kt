b81cc4a64f923a1fa73bcb3fa254e9375dabb3fb
YordanDeVp <yordanyrsm@gmail.com>
2025-11-22 10:11:28 -0500

    add class Simplewidget

--- app/src/main/java/com/example/lab14_widgets/SimpleWidget.kt
+++ app/src/main/java/com/example/lab14_widgets/SimpleWidget.kt
@@ -1,2 +1,8 @@
 package com.example.lab14_widgets
 
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
+}
